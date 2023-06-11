454d135feaea302f7c14b01b00aac2c8776ab49d
Ash <webartboy@gmail.com>
2023-06-11 13:15:41 +0400

    llama stop token fix

--- public/script.js
+++ public/script.js
@@ -1920,7 +1920,7 @@ $(document).ready(function(){
                 getMessage = getMessage.replace(/\n+$/, "");
 
                 message_already_generated +=getMessage;
-                if(message_already_generated.indexOf('You:') === -1 && message_already_generated.indexOf(name1) === -1 && message_already_generated.indexOf('<|endoftext|>') === -1 && message_already_generated.indexOf('\\end{code}') === -1 && tokens_already_generated < parseInt(this_max_gen) && getMessage.length > 0){
+                if(message_already_generated.indexOf('You:') === -1 && message_already_generated.indexOf(name1) === -1 && message_already_generated.indexOf('<|endoftext|>') === -1 && message_already_generated.indexOf('\\end') === -1 && tokens_already_generated < parseInt(this_max_gen) && getMessage.length > 0){
                     runGenerate(getMessage);
                     return;
                 }
@@ -1944,8 +1944,8 @@ $(document).ready(function(){
                 getMessage = getMessage.substr(0,getMessage.indexOf('<|endoftext|>'));
 
             }
-            if(getMessage.indexOf('\\end{code}') != -1){
-                getMessage = getMessage.substr(0,getMessage.indexOf('\\end{code}'));
+            if(getMessage.indexOf('\\end') != -1){
+                getMessage = getMessage.substr(0,getMessage.indexOf('\\end'));
 
             }
             let this_mes_is_name = true;
